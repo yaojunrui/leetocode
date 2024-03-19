5414d6f24b190570f67617d75a7143b6b7ddc6bb
Junrui Yao <129917657+yaojunrui@users.noreply.github.com>
2024-03-18 23:06:30 -0700

    Time: 64 ms (58.81%), Space: 52.1 MB (10.87%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map;
+    for (var i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i);
+    }
+};
